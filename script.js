cc224692c739f64fa1120795e6474f6e3d2fe3d7
drewhouses <drewcasas@live.com>
2023-09-07 18:11:39 -0500

    Worked with tutor to help refactor code and finished the randomization portion, put everything into functions to create neater code

--- script.js
+++ script.js
@@ -7,62 +7,69 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword() {
-  var passwordLength = 0;
-
-  passwordLength = window.prompt("How many characters do you want in your password?");
-
-  while (passwordLength < 8 || passwordLength > 128) {
-    window.alert("Password length must be between 8 and 128 characters");
-
-    passwordLength = window.prompt("How many characters do you want in your password?");
+function getPasswordBase() {
+  var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0].toString();
+  var specialCharacters = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
+  var passwordBase = "";
+  var includeLowercase = confirm(
+    "Would you like to include lowercase letters?"
+  );
+  if (includeLowercase) {
+    passwordBase = passwordBase.concat(lowercaseLetters);
   }
-
-  var includeLowercase = confirm("Would you like to include lowercase letters?");
-  var includeUppercase = confirm("Would you like to include uppercase letters?");
+  var includeUppercase = confirm(
+    "Would you like to include uppercase letters?"
+  );
+  if (includeUppercase) {
+    passwordBase = passwordBase.concat(uppercaseLetters);
+  }
   var includeNumbers = confirm("Would you like to include numbers?");
-  var includeSpecialChars = confirm("Would you like to include special characters?");
 
-  if (!includeLowercase || !includeUppercase || !includeNumbers || !includeSpecialChars) {
-    alert("Invalid! At least one must be true");
-
-    includeLowercase = confirm("Would you like to include lowercase letters?");
-    includeUppercase = confirm("Would you like to include uppercase letters?");
-    includeNumbers = confirm("Would you like to include numbers?");
-    includeSpecialChars = confirm("Would you like to include special characters?");
+  if (includeNumbers) {
+    passwordBase = passwordBase.concat(numbers);
   }
+
+  var includeSpecialChars = confirm(
+    "Would you like to include special characters?"
+  );
+  if (includeSpecialChars) {
+    passwordBase = passwordBase.concat(specialCharacters);
+  }
+  if (passwordBase.length === 0) {
+    return getPasswordBase();
+  }
+  return passwordBase;
+}
+
+function getPasswordLength() {
+  var passwordLength = window.prompt(
+    "How many characters do you want in your password?"
+  );
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    return getPasswordLength();
+  }
+  return passwordLength;
+}
+
+function generatePassword() {
+  var passwordLength = getPasswordLength();
+  var passwordBase = getPasswordBase();
 
   var password = "";
-  var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-  var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numbers = [1,2,3,4,5,6,7,8,9,0];
-  var specialCharacters = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
+  for (let i = 0; i < passwordLength; i++) {
+    var randomChar = passwordBase.charAt(
+      Math.floor(Math.random() * passwordBase.length)
+    );
+    password = password.concat(randomChar);
+  }
 
-  
+  return password;
 }
-
-
-/*
-
-to implement randomization
-
-if (includeLowercase === true) {
-  append character to password string variable or maybe use array
-}
-
-This will be modularized to includeUppercase, includeNumbers etc.
-
-In the 'if' statement use Math.random to randomly select a character from the following variables:
-  var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-  var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var specialCharacters = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
-
-
-
-*/ 
